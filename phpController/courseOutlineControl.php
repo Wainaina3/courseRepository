98b33d1440a2eeafe3f7693b5c1f0428c2a52cff
Wainaina3 <dwainaina3@gmail.com>
2015-11-14 05:15:56 +0000

    I have added functions to delete course outline

--- phpController/courseOutlineControl.php
+++ phpController/courseOutlineControl.php
@@ -10,5 +10,46 @@ include_once(dirname(__FILE__)."\..\phpModel\courseOutline.php");
 
 class courseOutlineControl extends courseOutline
 {
+/*
+ * This functions get a course to be deleted from the user request
+ * It first checks whether the course id was set and only proceeds if it was set
+ * It then sends it to courseOutline to delete it
+ * @return boolean Returns true if it was able to delete the course outline
+ */
+    function deleteCourse()
+    {
+        if (!isset($_REQUEST['courseId'])) {
+           return false;
+        }
+
+        $courseId=$_REQUEST['courseId'];
+
+        return $this->deleteCourseOutline($courseId);
+    }
+}
+/*
+ * Creates an object of courseOutlineControl class
+ * Object will be used to call its functions
+ */
+$outlineController=new courseOutlineControl();
+/*
+ * Checks whether a command was set
+ * It will run the specific methods if there was a command sent to it
+ *
+ */
+if (isset($_REQUEST['cmd'])) {
+    $cmd=$_REQUEST['cmd'];
+
+    switch ($cmd) {
+
+        case 5:
+            $outlineController->deleteCourse();
+            break;
+
+        default:
+
+            break;
+    }
+
 
 }
--- phpModel/courseOutline.php
+++ phpModel/courseOutline.php
@@ -10,4 +10,16 @@ include_once("adb.php");
 class courseOutline extends adb
 {
 
+    /*
+     * This function deletes a course outline from the database
+     * It makes use of the adb class
+     * @param string $courseId The id of the course outline to be deleted
+     * @return boolean returns true if the outline was deleted
+     */
+    function deleteCourseOutline($courseId)
+    {
+        $sql="delete from courseoutline where courseId='$courseId'";
+
+        return $this->query($sql);
+    }
 }
