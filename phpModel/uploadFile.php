407a33f36ea4ef6aac578b1a5bb88eef1455b48c
Wainaina3 <dwainaina3@gmail.com>
2015-11-16 13:28:45 +0000

    David branch up to date

--- phpModel/uploadFile.php
+++ phpModel/uploadFile.php
@@ -10,4 +10,10 @@ include_once("adb.php");
 class uploadFile extends adb
 {
 
+    function insertFile($courseId, $courseTitle,$semester, $facultyId, $fileId, $storageLink) {
+
+        $sql="insert into pdffiletable set courseId='$courseId',courseTitle='$courseTitle',semester='$semester',facultyId='$facultyId',fileId='$fileId',storageLink='$storageLink'";
+
+        return $this->query($sql);
+    }
 }
