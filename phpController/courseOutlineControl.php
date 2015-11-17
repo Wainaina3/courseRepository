1a27ceafe1d6043fba4da62c458e6a7a0266510c
Akpene Hogar <akpenehoggar@gmail.com>
2015-11-17 16:20:59 +0000

    Work updated

--- phpController/courseOutlineControl.php
+++ phpController/courseOutlineControl.php
@@ -10,5 +10,47 @@ include_once(dirname(__FILE__)."\..\phpModel\courseOutline.php");
 
 class courseOutlineControl extends courseOutline
 {
+/*Akpene
+ *Adding a course outline
+ */
+public function addCourseControl(){
+						<p><div> <div class='headers'> Course Objectives </div><textarea class='css' col="10" row="20" id="course_objective" name="course_objective"></textarea></div>
+						</p>
+						<p><div> <div class='headers'> Course Description </div> <textarea class='css' col="10" row="20" id="course_description" name="course_description"></textarea></div>
+						</p>
+						<div> <div class='headers'> Learning Goals </div> <textarea class='css' col="20" row="10" id="learning_goals" name="learning_goals"></textarea></div>
+						<p>
+							//<div class='headers' id="scheduler" name="scheduler"> Course Schedule 
 
+								//<table id="schedule" name="schedule" border="1" width="100%" align="center">
+									//<div id="schedule_header" name="schedule_header"> <tr id='table_headers' name='table_headers'> 
+										//<td><b>Weeks</b></td>
+										//<td><b>Topics</b></td>
+										//<td><b>Readings</b></td>
+										//<td><b>Milestones</b></td>
+									//</tr></div>
+								//</table></div>
+
+								<div> <input type="button" onclick="addRow()" value="Add Row"></div>
+							</p>
+							<p>
+								<div> <div class='headers'> Course Evaluations </div> <textarea class='ckeditor' col="10" row="10" id="course_evaluations" name="course_evaluations"></textarea></div>
+							</p>
+							<p>
+								<div> <div class='headers'> Course Readings </div> <textarea class='ckeditor' col="10" row="10" id="course_readings" name="course_readings"></textarea></div>
+							</p>
+							<div> <input type="submit" value="Save"></div>
+						</form>
+						
+					</div>
+				</td>
+			</tr>
+
+		</table>
+		<p id="footer" >
+					Copyright &copy Webtech Group 9
+				</p>
+
+	</div>
+}
 }
--- phpModel/courseOutline.php
+++ phpModel/courseOutline.php
@@ -10,4 +10,18 @@ include_once("adb.php");
 class courseOutline extends adb
 {
 
+/*Akpene
+ *Date:14/11/2015
+ */
+//Adding a course outline
+public function addCourse($course_name,$course_id,$course_objective,
+			$course_readings,$course_description, $course_evaluation, $learning_goals,$courseDept){
+//connecting to the database
+			$this->connect();
+//sql injections
+			$sqlScript="insert into course_outline set course_id='$course_id',course_name='$course_name', learning_goals='$learning_goals',
+			course_objectives='$course_objective', course_descriptions='$course_description', course_readings='$course_readings',course_evaluations='$course_evaluation',course_dept='$courseDept'";
+		
+			return $this->query($sqlScript);
+		}
 }
